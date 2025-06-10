17e39f81f73632b7f4edf5ec40a709968d9ee204
Alexandre Alapetite <alexandre@alapetite.fr>
2025-06-10 22:16:09 +0200

    i18n: en-US

--- app/i18n/en-us/sub.php
+++ app/i18n/en-us/sub.php
@@ -50,7 +50,7 @@ return array(
 			'password' => 'HTTP password',	// IGNORE
 			'username' => 'HTTP username',	// IGNORE
 		),
-		'change_favicon' => 'Change...',	// TODO
+		'change_favicon' => 'Change...',	// IGNORE
 		'clear_cache' => 'Always clear cache',	// IGNORE
 		'content_action' => array(
 			'_' => 'Content action when fetching the article content',	// IGNORE
@@ -81,7 +81,7 @@ return array(
 		),
 		'http_headers' => 'HTTP Headers',	// IGNORE
 		'http_headers_help' => 'Headers are separated by a newline, and the name and value of a header are separated by a colon (e.g: <kbd><code>Accept: application/atom+xml<br />Authorization: Bearer some-token</code></kbd>).',	// IGNORE
-		'icon' => 'Icon',	// TODO
+		'icon' => 'Icon',	// IGNORE
 		'information' => 'Information',	// IGNORE
 		'keep_min' => 'Minimum number of articles to keep',	// IGNORE
 		'kind' => array(
@@ -214,7 +214,7 @@ return array(
 		),
 		'proxy' => 'Set a proxy for fetching this feed',	// IGNORE
 		'proxy_help' => 'Select a protocol (e.g: SOCKS5) and enter the proxy address (e.g: <kbd>127.0.0.1:1080</kbd> or <kbd>username:password@127.0.0.1:1080</kbd>)',	// IGNORE
-		'reset_favicon' => 'Reset to default',	// TODO
+		'reset_favicon' => 'Reset to default',	// IGNORE
 		'selector_preview' => array(
 			'show_raw' => 'Show source code',	// IGNORE
 			'show_rendered' => 'Show content',	// IGNORE
